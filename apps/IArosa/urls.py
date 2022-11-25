f0687758e6a5d33f91612ab28bc1ea34cb8fbc92
Felix <felixascencio99@gmail.com>
2022-11-25 13:03:39 -0600

    Actualizacion final

--- apps/IArosa/urls.py
+++ apps/IArosa/urls.py
@@ -5,6 +5,11 @@ app_name = 'IArosa'
 
 urlpatterns = [
     path(
+        '', 
+        views.LandingPageView.as_view(),
+        name='index'
+    ),
+    path(
         'map/<month>/<year>', 
         views.Map1View.as_view(),
         name='map'
--- apps/IArosa/views.py
+++ apps/IArosa/views.py
@@ -4,26 +4,34 @@ from django.views.generic import View, CreateView, ListView, TemplateView, Updat
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from unipath import Path
+import random
 
 # Create your views here.
-class NameView(TemplateView):
+class LandingPageView(ListView):
     # la nueva direccion de los template es Album/----.html
-    template_name = 'example.html'
+    template_name = 'landingPage.html'
+    context_object_name = 'fx'
+    def get_queryset(self):
+        return random.randint(1, 20), random.randint(1, 20)
 
 class Map1View(ListView):
     template_name = 'IAmap.html'
     context_object_name = 'maps'
 
     def get_queryset(self):
+        dataAI = [
+            ['2022', 'Septiembre', '1,0,1,0,0,1,0,0,0,1,0,0,1,0,1,1'],
+            ['2022', 'Octubre', '0,0,0,0,0,0,0,0,0,1,0,0,1,0,1,1'],
+            ['2022', 'Noviembre', '0,0,0,0,0,1,0,0,0,0,0,0,1,0,1,1'],
+        ]
         pkMonth = self.kwargs['month']
         pkYear = self.kwargs['year']
-        month = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-        years = [2018, 2019, 2020, 2021, 2022, 2023]
+        month = ['Septiembre', 'Octubre', 'Noviembre']
+        years = ['2022']
         # peticion o logica
-        alc = []
-        for x in range(16):
-            alc.append(['alcadia1', x+1])
-        query = ['anio', 'mes', alc]
+        for x in dataAI:
+            if x[0] == pkYear and x[1] == pkMonth:
+                query = x[2].split(",")
         return query, month, years
 
 def error_404_view(request, exception):
